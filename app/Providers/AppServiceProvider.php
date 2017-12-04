8edbbbc9c9f4d0f38b636918d2821f6346a1f147
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2017-12-04 11:55:06 +0100

    Add register logic to AppServiceProvider for development env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,15 @@
 
 namespace App\Providers;
 
+
+use Barryvdh\Debugbar\Facade;
+use Barryvdh\Debugbar\ServiceProvider as DebugBarServiceProvider;
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
+use Carbon\Carbon;
+use Faker\Factory as FakerFactory;
+use Faker\Generator as FakerGenerator;
 use Illuminate\Support\ServiceProvider;
+use Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,9 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
-
+    
     /**
      * Register any application services.
      *
@@ -23,6 +31,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->setLocales();
+        $this->registerDevProviders();
     }
+    
+    private function setLocales()
+    {
+        setlocale(LC_ALL, config("app.locale_extended"));
+        Carbon::setLocale(config("app.locale"));
+    }
+    
+    private function registerDevProviders()
+    {
+        if ($this->app->environment("local", "testing", "staging", "development", "dev")) {
+            $this->app->register(DebugBarServiceProvider::class);
+            $this->app->register(IdeHelperServiceProvider::class);
+            $this->app->alias("Debugbar", Facade::class);
+            
+            $this->app->singleton(FakerGenerator::class, function () {
+                return FakerFactory::create(config("app.locale_extended"));
+            });
+        }
+    }
+    
 }
